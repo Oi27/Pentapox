39d601fafd722cd5a997eaac97f17d396fb944da
Scott Evans <sevans@cbpmail.onmicrosoft.com>
2023-11-13 13:41:21 -0500

    Add project files.

---
+++ Pentapox/PentaPox.Designer.cs
@@ -0,0 +1,327 @@
+﻿
+namespace Pentapox
+{
+    partial class PentaPox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ColorPreview = new System.Windows.Forms.PictureBox();
+            this.RedValue = new System.Windows.Forms.TextBox();
+            this.GreenValue = new System.Windows.Forms.TextBox();
+            this.BlueValue = new System.Windows.Forms.TextBox();
+            this.PentaMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadpalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutPenta = new System.Windows.Forms.ToolStripMenuItem();
+            this.PentaVersion = new System.Windows.Forms.ToolStripMenuItem();
+            this.FiveBPP = new System.Windows.Forms.TextBox();
+            this.TwentyFourBPP = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.RedScrollBar = new System.Windows.Forms.TrackBar();
+            this.GreenScrollBar = new System.Windows.Forms.TrackBar();
+            this.BlueScrollBar = new System.Windows.Forms.TrackBar();
+            this.ColorValueSliders = new System.Windows.Forms.Panel();
+            this.ColorValueBoxes = new System.Windows.Forms.FlowLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.ColorPreview)).BeginInit();
+            this.PentaMenuStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.RedScrollBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GreenScrollBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BlueScrollBar)).BeginInit();
+            this.ColorValueSliders.SuspendLayout();
+            this.ColorValueBoxes.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "R";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "G";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "B";
+            // 
+            // ColorPreview
+            // 
+            this.ColorPreview.Location = new System.Drawing.Point(169, 50);
+            this.ColorPreview.Name = "ColorPreview";
+            this.ColorPreview.Size = new System.Drawing.Size(35, 58);
+            this.ColorPreview.TabIndex = 6;
+            this.ColorPreview.TabStop = false;
+            // 
+            // RedValue
+            // 
+            this.RedValue.Location = new System.Drawing.Point(3, 3);
+            this.RedValue.MaxLength = 2;
+            this.RedValue.Name = "RedValue";
+            this.RedValue.Size = new System.Drawing.Size(34, 20);
+            this.RedValue.TabIndex = 7;
+            this.RedValue.Tag = "R";
+            this.RedValue.Text = "00";
+            this.RedValue.Leave += new System.EventHandler(this.ColorValueBox_Leave);
+            // 
+            // GreenValue
+            // 
+            this.GreenValue.Location = new System.Drawing.Point(3, 29);
+            this.GreenValue.MaxLength = 2;
+            this.GreenValue.Name = "GreenValue";
+            this.GreenValue.Size = new System.Drawing.Size(34, 20);
+            this.GreenValue.TabIndex = 8;
+            this.GreenValue.Tag = "G";
+            this.GreenValue.Text = "00";
+            this.GreenValue.Leave += new System.EventHandler(this.ColorValueBox_Leave);
+            // 
+            // BlueValue
+            // 
+            this.BlueValue.Location = new System.Drawing.Point(3, 55);
+            this.BlueValue.MaxLength = 2;
+            this.BlueValue.Name = "BlueValue";
+            this.BlueValue.Size = new System.Drawing.Size(34, 20);
+            this.BlueValue.TabIndex = 9;
+            this.BlueValue.Tag = "B";
+            this.BlueValue.Text = "00";
+            this.BlueValue.Leave += new System.EventHandler(this.ColorValueBox_Leave);
+            // 
+            // PentaMenuStrip
+            // 
+            this.PentaMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutPenta});
+            this.PentaMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.PentaMenuStrip.Name = "PentaMenuStrip";
+            this.PentaMenuStrip.Size = new System.Drawing.Size(228, 24);
+            this.PentaMenuStrip.TabIndex = 10;
+            this.PentaMenuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadpalToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // loadpalToolStripMenuItem
+            // 
+            this.loadpalToolStripMenuItem.Enabled = false;
+            this.loadpalToolStripMenuItem.Name = "loadpalToolStripMenuItem";
+            this.loadpalToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.loadpalToolStripMenuItem.Text = "Load .pal";
+            // 
+            // aboutPenta
+            // 
+            this.aboutPenta.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.PentaVersion});
+            this.aboutPenta.Name = "aboutPenta";
+            this.aboutPenta.Size = new System.Drawing.Size(52, 20);
+            this.aboutPenta.Text = "About";
+            // 
+            // PentaVersion
+            // 
+            this.PentaVersion.Enabled = false;
+            this.PentaVersion.Name = "PentaVersion";
+            this.PentaVersion.Size = new System.Drawing.Size(180, 22);
+            this.PentaVersion.Text = "PentaPox v.0.0.1";
+            // 
+            // FiveBPP
+            // 
+            this.FiveBPP.Location = new System.Drawing.Point(29, 130);
+            this.FiveBPP.MaxLength = 4;
+            this.FiveBPP.Name = "FiveBPP";
+            this.FiveBPP.Size = new System.Drawing.Size(47, 20);
+            this.FiveBPP.TabIndex = 11;
+            this.FiveBPP.Text = "0000";
+            this.FiveBPP.Leave += new System.EventHandler(this.FiveBPP_Leave);
+            // 
+            // TwentyFourBPP
+            // 
+            this.TwentyFourBPP.Location = new System.Drawing.Point(101, 130);
+            this.TwentyFourBPP.MaxLength = 6;
+            this.TwentyFourBPP.Name = "TwentyFourBPP";
+            this.TwentyFourBPP.Size = new System.Drawing.Size(57, 20);
+            this.TwentyFourBPP.TabIndex = 12;
+            this.TwentyFourBPP.Text = "000000";
+            this.TwentyFourBPP.Leave += new System.EventHandler(this.TwentyFourBPP_Leave);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "5-Bit BGR";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(98, 114);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "24-Bit RGB";
+            // 
+            // RedScrollBar
+            // 
+            this.RedScrollBar.Location = new System.Drawing.Point(3, 4);
+            this.RedScrollBar.Maximum = 31;
+            this.RedScrollBar.Name = "RedScrollBar";
+            this.RedScrollBar.Size = new System.Drawing.Size(76, 45);
+            this.RedScrollBar.TabIndex = 15;
+            this.RedScrollBar.TabStop = false;
+            this.RedScrollBar.Tag = "R";
+            this.RedScrollBar.TickFrequency = 0;
+            this.RedScrollBar.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.RedScrollBar.Scroll += new System.EventHandler(this.ColorBar_Scroll);
+            // 
+            // GreenScrollBar
+            // 
+            this.GreenScrollBar.Location = new System.Drawing.Point(3, 21);
+            this.GreenScrollBar.Maximum = 31;
+            this.GreenScrollBar.Name = "GreenScrollBar";
+            this.GreenScrollBar.Size = new System.Drawing.Size(76, 45);
+            this.GreenScrollBar.TabIndex = 16;
+            this.GreenScrollBar.TabStop = false;
+            this.GreenScrollBar.Tag = "G";
+            this.GreenScrollBar.TickFrequency = 0;
+            this.GreenScrollBar.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.GreenScrollBar.Scroll += new System.EventHandler(this.ColorBar_Scroll);
+            // 
+            // BlueScrollBar
+            // 
+            this.BlueScrollBar.Location = new System.Drawing.Point(3, 37);
+            this.BlueScrollBar.Maximum = 31;
+            this.BlueScrollBar.Name = "BlueScrollBar";
+            this.BlueScrollBar.Size = new System.Drawing.Size(76, 45);
+            this.BlueScrollBar.TabIndex = 17;
+            this.BlueScrollBar.TabStop = false;
+            this.BlueScrollBar.Tag = "B";
+            this.BlueScrollBar.TickFrequency = 0;
+            this.BlueScrollBar.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.BlueScrollBar.Scroll += new System.EventHandler(this.ColorBar_Scroll);
+            // 
+            // ColorValueSliders
+            // 
+            this.ColorValueSliders.Controls.Add(this.BlueScrollBar);
+            this.ColorValueSliders.Controls.Add(this.GreenScrollBar);
+            this.ColorValueSliders.Controls.Add(this.RedScrollBar);
+            this.ColorValueSliders.Location = new System.Drawing.Point(40, 46);
+            this.ColorValueSliders.Name = "ColorValueSliders";
+            this.ColorValueSliders.Size = new System.Drawing.Size(80, 61);
+            this.ColorValueSliders.TabIndex = 18;
+            // 
+            // ColorValueBoxes
+            // 
+            this.ColorValueBoxes.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ColorValueBoxes.Controls.Add(this.RedValue);
+            this.ColorValueBoxes.Controls.Add(this.GreenValue);
+            this.ColorValueBoxes.Controls.Add(this.BlueValue);
+            this.ColorValueBoxes.Location = new System.Drawing.Point(122, 39);
+            this.ColorValueBoxes.Name = "ColorValueBoxes";
+            this.ColorValueBoxes.Size = new System.Drawing.Size(41, 85);
+            this.ColorValueBoxes.TabIndex = 19;
+            // 
+            // PentaPox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(228, 193);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.ColorValueBoxes);
+            this.Controls.Add(this.ColorValueSliders);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TwentyFourBPP);
+            this.Controls.Add(this.FiveBPP);
+            this.Controls.Add(this.ColorPreview);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PentaMenuStrip);
+            this.MainMenuStrip = this.PentaMenuStrip;
+            this.Name = "PentaPox";
+            this.Text = "PentaPox";
+            ((System.ComponentModel.ISupportInitialize)(this.ColorPreview)).EndInit();
+            this.PentaMenuStrip.ResumeLayout(false);
+            this.PentaMenuStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.RedScrollBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GreenScrollBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BlueScrollBar)).EndInit();
+            this.ColorValueSliders.ResumeLayout(false);
+            this.ColorValueSliders.PerformLayout();
+            this.ColorValueBoxes.ResumeLayout(false);
+            this.ColorValueBoxes.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox ColorPreview;
+        private System.Windows.Forms.TextBox RedValue;
+        private System.Windows.Forms.TextBox GreenValue;
+        private System.Windows.Forms.TextBox BlueValue;
+        private System.Windows.Forms.MenuStrip PentaMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutPenta;
+        private System.Windows.Forms.TextBox FiveBPP;
+        private System.Windows.Forms.TextBox TwentyFourBPP;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TrackBar RedScrollBar;
+        private System.Windows.Forms.TrackBar GreenScrollBar;
+        private System.Windows.Forms.TrackBar BlueScrollBar;
+        private System.Windows.Forms.Panel ColorValueSliders;
+        private System.Windows.Forms.FlowLayoutPanel ColorValueBoxes;
+        private System.Windows.Forms.ToolStripMenuItem loadpalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem PentaVersion;
+    }
+}
+
---
+++ Pentapox/PentaPox.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pentapox
+{
+    public partial class PentaPox : Form
+    {
+        public PentaPox()
+        {
+            InitializeComponent();
+            UpdateColorCodeBoxes();
+        }
+
+        private FiveBitColor ColorFromScrollBars()
+        {
+            byte R = (byte)RedScrollBar.Value;
+            byte G = (byte)GreenScrollBar.Value;
+            byte B = (byte)BlueScrollBar.Value;
+            return new FiveBitColor(R, G, B);
+        }
+
+        private void ColorBar_Scroll(object sender, EventArgs e)
+        {
+            TrackBar A = (TrackBar)sender;
+            UpdateColorCodeBoxes();
+            UpdateColorValueBoxes();
+        }
+
+        private void UpdateColorValueBoxes()
+        {
+            foreach (Control item in ColorValueSliders.Controls)
+            {
+                TrackBar slider = (TrackBar)item;
+                foreach (Control box in ColorValueBoxes.Controls)
+                {
+                    //locate the control with the matching tag and propagate values
+                    if(box.Tag == slider.Tag)
+                    {
+                        box.Text = slider.Value.ToString("X2");
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void UpdateColorValueSlidersFromColorValueBoxes()
+        {
+            foreach (Control item in ColorValueBoxes.Controls)
+            {
+                TextBox box = (TextBox)item;
+                foreach (Control item2 in ColorValueSliders.Controls)
+                {
+                    //locate the control with the matching tag and propagate values
+                    TrackBar slider = (TrackBar)item2;
+                    if (box.Tag == slider.Tag)
+                    {
+                        slider.Value = int.Parse(box.Text, System.Globalization.NumberStyles.HexNumber);
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void UpdateColorCodeBoxes()
+        {
+            FiveBitColor color = ColorFromScrollBars();
+            FiveBPP.Text = color.ToFiveBGR().ToString("X4");
+            TwentyFourBPP.Text =
+                color.To24BPP().R.ToString("X2") +
+                color.To24BPP().G.ToString("X2") +
+                color.To24BPP().B.ToString("X2");
+            UpdatePreview(color);
+        }
+
+        private void UpdateColorValueSliders(FiveBitColor color)
+        {
+            RedScrollBar.Value = color.Red;
+            GreenScrollBar.Value = color.Green;
+            BlueScrollBar.Value = color.Blue;
+            UpdateColorValueBoxes();
+        }
+
+        private void ColorValueBox_Leave(object sender, EventArgs e)
+        {
+            UpdateColorValueSlidersFromColorValueBoxes();
+            UpdateColorCodeBoxes();
+        }
+
+        private void FiveBPP_Leave(object sender, EventArgs e)
+        {
+            FiveBitColor color = new FiveBitColor(short.Parse(FiveBPP.Text, System.Globalization.NumberStyles.HexNumber));
+            UpdateColorValueSliders(color);
+            UpdateColorCodeBoxes();
+        }
+
+        private void TwentyFourBPP_Leave(object sender, EventArgs e)
+        {
+            Color newColor = Color.FromArgb(int.Parse(TwentyFourBPP.Text, System.Globalization.NumberStyles.HexNumber));
+            FiveBitColor color = new FiveBitColor(newColor);
+            UpdateColorValueSliders(color);
+            UpdateColorCodeBoxes();
+        }
+
+        private void UpdatePreview(FiveBitColor color)
+        {
+            int width = 20;
+            int height = 20;
+            ColorPreview.Image = color.ToImage(width, height);
+            ColorPreview.Width = width;
+            ColorPreview.Height = height;
+        }
+    }
+
+    public class FiveBitColor
+    {
+        public FiveBitColor(byte R, byte G, byte B)
+        {
+            //byte data type to construct from 5BPP data 00-1F
+            //if any numbers invalid black out all of them
+            bool colorsValid = (R > 32) && (G > 32) && (B > 32);
+            if (!colorsValid) 
+            {
+                Red = 0;
+                Green = 0;
+                Blue = 0;
+            }
+            Red = R;
+            Green = G;
+            Blue = B;
+        }
+        public FiveBitColor(Color RGB)
+        {
+            //24bit color
+            Red = RGB.R / 8;
+            Green = RGB.G / 8;
+            Blue = RGB.B / 8;
+        }
+        public FiveBitColor(short RGB)
+        {
+            //0bbb bbgg gggr rrrr 
+            Red = RGB & 0x001F;
+            Green = (RGB & 0x03E0) >> 5;
+            Blue = (RGB & 0x7C00) >> 10;
+        }
+        public int ToFiveBGR()
+        {
+            return Red | (Green << 5) | (Blue << 10);
+        }
+        public Color To24BPP()
+        {
+            return Color.FromArgb(Red * 8, Green * 8, Blue * 8);
+        }
+        public Bitmap ToImage(int width, int height)
+        {
+            Bitmap image = new Bitmap(width, height);
+            Color thisColor = this.To24BPP();
+            for(int x = 0; x < image.Width; x++)
+            {
+                for(int y = 0; y < image.Height; y++)
+                {
+                    image.SetPixel(x, y, thisColor);
+                }
+            }
+            return image;
+        }
+        public int Red { set; get; }
+        public int Green { set; get; }
+        public int Blue { set; get; }
+    }
+}
